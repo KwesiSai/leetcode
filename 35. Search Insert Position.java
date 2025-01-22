400b6d530534c43d85207677684d899e3a06d542
Kwesi Abraham <83294945+KwesiSai@users.noreply.github.com>
2025-01-22 14:06:44 +0000

    Binary search with O(log n) complexity

--- 35. Search Insert Position.java
+++ 35. Search Insert Position.java
@@ -11,3 +11,19 @@ class Solution {
         return index;
     }
 }
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Move to the right half
+            } else {
+                right = mid - 1; // Move to the left half
+            }
+        }
+        return left; // Insert position
+    }
+}
